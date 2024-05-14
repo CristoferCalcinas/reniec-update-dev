1b99f25e1d03454a95691eb92390f90dcc4b8c2f
adrian <adrian@google.com>
2024-05-14 14:26:44 -0400

    arreglando el login

--- src/auth/page/LoginPage.jsx
+++ src/auth/page/LoginPage.jsx
@@ -1,14 +1,14 @@
 export const LoginPage = () => {
   return (
-    <main>
+    <main className="h-screen">
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className=" sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            className="mx-auto h-10 w-auto rounded-md"
+            src="https://cdn6.aptoide.com/imgs/c/8/2/c82d88a3bf5ce9285718a734434b9ea9_icon.png"
             alt="RENIEC Logo"
           />
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          <h2 className="mt-10 text-center text-4xl font-bold leading-9 tracking-tight text-gray-900">
             Iniciar Sesión
           </h2>
         </div>
@@ -18,7 +18,7 @@ export const LoginPage = () => {
             <div>
               <label
                 htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                className="block text-lg font-medium leading-6 text-gray-900"
               >
                 Ingrese su DNI
               </label>
@@ -38,14 +38,14 @@ export const LoginPage = () => {
               <div className="flex items-center justify-between">
                 <label
                   htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className="block text-lg font-medium leading-6 text-gray-900"
                 >
                   Contraseña
                 </label>
                 <div className="text-sm">
                   <a
                     href="#"
-                    className="font-semibold text-indigo-600 hover:text-indigo-500"
+                    className="font-semibold text-red-800 hover:text-red-600"
                   >
                     Olvidaste tu contraseña?
                   </a>
@@ -66,7 +66,7 @@ export const LoginPage = () => {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="flex w-full justify-center rounded-md bg-red-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
               >
                 Iniciar Sesión
               </button>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -4,7 +4,7 @@ import { CalendarPage } from "../calendar";
 import { PrincipalPage } from "../principal";
 
 export const AppRouter = () => {
-  const authStatus = "authenticated"; //"authenticated"; //"not-authenticated";
+  const authStatus = "not-authenticated"; //"authenticated"; //"not-authenticated";
   return (
     <Routes>
       {authStatus === "not-authenticated" ? (
