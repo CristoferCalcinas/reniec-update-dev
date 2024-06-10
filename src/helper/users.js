3ef15d7ff40d7265a8b1e33f53e526cbde8df6c3
adrian <adrian@google.com>
2024-06-10 11:55:28 -0400

    agreangado usuario de prueba

---
+++ src/helper/users.js
@@ -0,0 +1,39 @@
+// este archivo contiene usuarios falsos pero serviran de prueba para la posterior conecion con la base de datos
+
+export const UsersInPlatform = [
+    {
+        id: 1,
+        name: 'Luis Santolalla',
+        dni: '42189197-0',
+        password: '42189197',
+        role: 'admin',
+    },
+    {
+        id: 2,
+        name: 'Ana Contreras',
+        dni: '70025425-8',
+        password: '70025425',
+        role: 'admin',
+    },
+    {
+        id: 3,
+        name: 'Vanessa Castillo',
+        dni: '43451826-7',
+        password: '43451826',
+        role: 'civil',
+    },
+    {
+        id: 4,
+        name: 'Jose Tomas',
+        dni: '17859853-3',
+        password: '17859853',
+        role: 'civil',
+    },
+    {
+        id: 5,
+        name: 'Rosa Maria',
+        dni: '12345678-9',
+        password: '12345678',
+        role: 'civil',
+    },
+];
